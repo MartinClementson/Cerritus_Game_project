2525808a1d0d6444176bb93f6766ed0a5c42e6bc
Gaffel15 <thegaffel15@gmail.com>
2016-04-14 12:02:45 +0200

    adding stubbings

--- GameEngine/ParticleSystem.h
+++ GameEngine/ParticleSystem.h
@@ -1,8 +1,22 @@
 #pragma once
+#include "LibIncluder.h"
+#include "Projectile.h"
+
 class ParticleSystem
 {
 public:
 	ParticleSystem();
 	~ParticleSystem();
+	
+private:
+	std::vector<Projectile> projectiles;
+	float lifeSpan;
+	int maxProjectiles;
+
+public:
+	Projectile FireProjectile(DirectX::XMFLOAT3 origin, DirectX::XMFLOAT3 direction);
+	void UpdateParticle();
+	void Initialize();
+	void Release();
 };
 
