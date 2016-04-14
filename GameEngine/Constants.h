bce10ecb3710d72fd01ab2a00b4fd334a257f573
MartinClementson <martinc.91@hotmail.com>
2016-04-14 10:59:01 +0200

    Added constants

--- GameEngine/Constants.h
+++ GameEngine/Constants.h
@@ -1,5 +1,13 @@
 #pragma once
 #define WIN_WIDTH 800.0f
 #define WIN_HEIGHT 600.0f
-#define DEBUG 0;
+
+#define WINDOWED true
+#define WIREFRAME false
+#define VSYNC 1 //1 == ON, 0 = OFF
+
+#define DEBUG 0 /* 2 = debug,  or : D3D11_CREATE_DEVICE_DEBUG */
 #define DIRECTINPUT_VERSION 0x0800
+#define D3D_DEBUG_INFO
+
+#define SAFE_RELEASE(p) { if ( (p) ) { (p)->Release(); (p) = 0; } }
