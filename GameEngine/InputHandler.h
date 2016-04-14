fa76aa2dad951ddff7515929077c958e2a2aff43
Gaffel15 <thegaffel15@gmail.com>
2016-04-14 10:52:02 +0200

    stuff

--- GameEngine/InputHandler.h
+++ GameEngine/InputHandler.h
@@ -4,5 +4,23 @@ class InputHandler
 public:
 	InputHandler();
 	~InputHandler();
+private:
+	//LPDIRECTINPUT8 input;
+	//LPDIRECTINPUTDEVICE8 keyboard;
+	//LPDIRECTINPUTDEVICE8 mouse;
+	unsigned char keyboardState[256];
+	//DIMOUSESTATE mouseState;
+
+	float mouseX, mouseY;
+	float lastMouseX, lastMouseY;
+
+private:
+	void ProcessInput();
+	bool ReadKeyboard();
+	bool ReadMouse();
+
+	//HWND hwndP;
+public:
+
 };
 
