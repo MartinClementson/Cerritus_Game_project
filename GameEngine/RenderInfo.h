1c460acf802d3e90b104f9f9e16563ad4fadae08
MartinClementson <martinc.91@hotmail.com>
2016-04-14 12:03:23 +0200

    added RenderInfoObject

--- GameEngine/RenderInfo.h
+++ GameEngine/RenderInfo.h
@@ -1,1 +1,12 @@
 #pragma once
+#include "LibIncluder.h"
+
+using namespace DirectX;
+
+struct RenderInfoObject {
+	XMFLOAT3 position;
+	XMFLOAT3 rotation;
+
+	// Objects object
+	// Shader shader		//these are enum
+};
