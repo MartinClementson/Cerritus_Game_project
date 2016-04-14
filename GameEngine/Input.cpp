bb185a4be2d455a450272bb83fea6caf4e9764e8
Gaffel15 <thegaffel15@gmail.com>
2016-04-14 11:20:00 +0200

    added comment

--- GameEngine/Input.cpp
+++ GameEngine/Input.cpp
@@ -20,22 +20,22 @@ void Input::Release()
 {
 }
 
-bool Input::IsKeyPressed(InputKeys * key)
-{
-	return false;
-}
-
-bool Input::IsKeyHeld(InputKeys * key)
-{
-	return false;
-}
-
-XMVECTOR2 Input::GetMousePosition()
-{
-	return XMVECTOR2();
-}
-
-bool Input::isMouseClicked(InputKeys * mouseKey)
-{
-	return false;
-}
+//bool Input::IsKeyPressed(InputKeys * key)
+//{
+//	return false;
+//}
+//
+//bool Input::IsKeyHeld(InputKeys * key)
+//{
+//	return false;
+//}
+//
+//XMVECTOR2 Input::GetMousePosition()
+//{
+//	return XMVECTOR2();
+//}
+//
+//bool Input::isMouseClicked(InputKeys * mouseKey)
+//{
+//	return false;
+//}
