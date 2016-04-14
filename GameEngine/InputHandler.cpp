b8517fae90aa2079d6c344ccf2eb716e9c1598f4
Gaffel15 <thegaffel15@gmail.com>
2016-04-14 10:54:46 +0200

    added stubbs

--- GameEngine/InputHandler.cpp
+++ GameEngine/InputHandler.cpp
@@ -10,3 +10,17 @@ InputHandler::InputHandler()
 InputHandler::~InputHandler()
 {
 }
+
+void InputHandler::ProcessInput()
+{
+}
+
+bool InputHandler::ReadKeyboard()
+{
+	return false;
+}
+
+bool InputHandler::ReadMouse()
+{
+	return false;
+}
