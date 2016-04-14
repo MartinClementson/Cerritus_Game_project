52ca6411cf21b4e27f74fb217f39e2dc463e3d53
MartinClementson <martinc.91@hotmail.com>
2016-04-14 17:03:17 +0200

    Implementing stubs in shader manager

--- GameEngine/ShaderManager.cpp
+++ GameEngine/ShaderManager.cpp
@@ -10,3 +10,44 @@ ShaderManager::ShaderManager()
 ShaderManager::~ShaderManager()
 {
 }
+
+void ShaderManager::Initialize(ID3D11Device * gDevice, ID3D11DeviceContext * gDeviceContext)
+{
+
+}
+
+void ShaderManager::Release()
+{
+}
+
+
+
+
+void ShaderManager::CreateShaders()
+{
+
+}
+
+
+#pragma region Create each individual shader
+
+bool ShaderManager::CreatePhongShader()
+{
+	return false;
+}
+
+bool ShaderManager::CreateAnimationShader()
+{
+	return false;
+}
+
+bool ShaderManager::CreateParticleShader()
+{
+	return false;
+}
+
+bool ShaderManager::CreateBillboardShader()
+{
+	return false;
+}
+#pragma endregion
