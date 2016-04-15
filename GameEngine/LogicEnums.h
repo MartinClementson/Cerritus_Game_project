f3f1b9848ad762fdf6bbaf505e4e7f07dffc0387
Gaffel15 <thegaffel15@gmail.com>
2016-04-15 09:32:48 +0200

    ADDED DAS ENUMS

---
+++ GameEngine/LogicEnums.h
@@ -0,0 +1,91 @@
+﻿#pragma once
+enum EnemyAnimations
+{
+	WALK,
+	DIE,
+	JUMP,
+	ATTACK
+};
+
+enum PlayerAnimation
+{
+	WALK,
+	DIE,
+	SHOOT
+};
+
+enum Meshes
+{
+	MAIN_CHARACTER,
+	ENEMY_1,
+	PROJECTILE_1,
+	TRAP_BEAR,
+	TRAP_FIRE
+};
+
+enum UITextures
+{
+	HEALTHBAR,
+	WAVECOUNTER
+};
+
+enum LightType
+{
+	POINTLIGHT,
+	AREALIGHT
+};
+
+enum Shaders
+{
+	PHONG_SHADER,
+	PARTICLE_SHADER,
+	BILLBOARD_SHADER,
+	UI_SHADER,
+	ANIMATION_SHADER
+};
+
+enum TrapState
+{
+	IDLE,
+	ACTIVE,
+	INACTIVE
+};
+
+enum EnemyState
+{
+	IDLE,
+	ATTACK,
+	DEATH
+};
+
+enum States
+{
+	MENU,
+	GAME,
+	GAMEOVER
+};
+
+enum InputKeys
+{
+	KEY_W,
+	KEY_A,
+	KEY_S,
+	KEY_D,
+	KEY_LEFT,
+	KEY_RIGHT,
+	KEY_UP,
+	KEY_DOWN,
+	KEY_SPACE,
+	KEY_ENTER,
+
+	MOUSE_RIGHT,
+	MOUSE_LEFT
+};
+
+enum TextureType
+{
+	DIFFUSE,
+	SPECULAR,
+	GLOW,
+	NORMAL
+};
