b4715ea26efc4e53ae7d77940d609c14e1fb4d42
Kaptenotto <johancarlphilipottosson@gmail.com>
2016-04-19 00:12:10 +0200

    Camera update

--- GameEngine/Camera.h
+++ GameEngine/Camera.h
@@ -1,1 +1,17 @@
 #pragma once
+#ifndef  CAMERA_H
+#define CAMERA_H
+
+class Camera
+{
+private:
+
+private:
+
+public:
+	Camera();
+	~Camera();
+};
+#endif // ! CAMERA_H
+
+
--- GameEngine/Structs/ConstantBufferStruct.h
+++ GameEngine/Structs/ConstantBufferStruct.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "../Source/LibIncluder.h"
+
+struct CamMatrices
+{
+	DirectX::XMMATRIX camView;
+	DirectX::XMMATRIX projection;
+};
+CamMatrices camMatrices;
+
+struct WorldMatrix
+{
+	DirectX::XMMATRIX worldMatrix;
+};
+WorldMatrix wordlMatrix;
