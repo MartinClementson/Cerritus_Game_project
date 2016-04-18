2d5d02e6a755ba6d493edaf36d6786787d501af3
surethang94 <surethang@live.com>
2016-04-18 15:11:35 +0200

    new

--- GameEngine/GameLogic/Character/ProjectileSystem/Projectile/Projectile.cpp
+++ GameEngine/GameLogic/Character/ProjectileSystem/Projectile/Projectile.cpp
@@ -13,7 +13,12 @@ Projectile::~Projectile()
 
 void Projectile::Initialize()
 {
+	age = 5.0f;
+	speed = 3.0f;
+	dmgMultiplier = 2.0f;
 
+	isFired = false;
+	colided = false;
 }
 
 void Projectile::Release()
--- GameEngine/GameLogic/Character/ProjectileSystem/ProjectileSystem.cpp
+++ GameEngine/GameLogic/Character/ProjectileSystem/ProjectileSystem.cpp
@@ -14,15 +14,22 @@ ProjectileSystem::~ProjectileSystem()
 Projectile ProjectileSystem::FireProjectile(DirectX::XMFLOAT3 origin, DirectX::XMFLOAT3 direction)
 { 
 
+	//origin from players pos towards where he is looking.
+
 	return Projectile();
 }
 
 void ProjectileSystem::UpdateParticle(double deltaTime)
 {
+
+
 }
 
 void ProjectileSystem::Initialize()
 {
+	lifeSpan = 2.5f;
+	maxProjectiles = 12;
+
 }
 
 void ProjectileSystem::Release()
