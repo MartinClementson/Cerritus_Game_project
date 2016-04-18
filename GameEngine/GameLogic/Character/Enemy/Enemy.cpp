f36aa3a4e375b113709b8b80d3ee2ccb946fa9ec
surethang94 <surethang@live.com>
2016-04-18 14:01:08 +0200

    Enemy no break

--- GameEngine/GameLogic/Character/Enemy/Enemy.cpp
+++ GameEngine/GameLogic/Character/Enemy/Enemy.cpp
@@ -1,7 +1,5 @@
 #include "Enemy.h"
 
-
-
 Enemy::Enemy()
 {
 
@@ -15,26 +13,25 @@ Enemy::~Enemy()
 
 void Enemy::Initialize()
 {
-
+	graphics = Graphics::GetInstance();
 	movementSpeed = 1.0f;
 
 	health = 100.0f;
 
 	damage = 22.0f;
-
+	position = { 0,0,0 };
+	rotation = { 0,0,0 };
 
 }
 
 void Enemy::Release()
 {
-
+	
 }
 
 void Enemy::Update(double deltaTime)
 {
-	
 	//enemy basic movement
-
 	//position of the enemy to test the movement of the enemy we set the start value at 0,0,0 and if the x value is 0 we move in that direction untill we reach x = 5 then we tail backwards to zero,
 	//just to see if the movement of the enemys work. 
 	if (position.x == 0)
@@ -46,28 +43,24 @@ void Enemy::Update(double deltaTime)
 		position.x = position.x - movementSpeed;
 	}
 	//just if we want to decrease the movement speed of the enemys when they are hurt. 
-	if (health == 100)
-	{
-		movementSpeed = 1.0f;
-	}
-	else if (health < 50)
-	{
-		movementSpeed = 0.75f;
-	}
-	if (health == 0)
-	{
-		//call for deathstate for the enemy..
-	}
-
-
-	
+	//if (health == 100)
+	//{
+	//	movementSpeed = 1.0f;
+	//}
+	//else if (health < 50)
+	//{
+	//	movementSpeed = 0.75f;
+	//}
+	//if (health == 0)
+	//{
+	//	//call for deathstate for the enemy..
+	//}	
 	//moving the enemy in the x coords with the speed of the enemy
 	
-
-
+	renderInfo = { position, rotation };
 }
 
 void Enemy::Render()
 {
-	
+	//graphics->QueueRender(renderInfo); doesnt work atm.
 }
--- GameEngine/GameLogic/Character/Enemy/Enemy.h
+++ GameEngine/GameLogic/Character/Enemy/Enemy.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "../Character.h"
 #include "../../StateMachines/EnemyStateMachine/EnemyStateMachine.h"
+#include "../../../Source//LibIncluder.h"
+#include "../../../Enumerations/Enumerations.h"
+#include "../../InputHandler/Input/Input.h"
+#include "../../../../GameEngine/Structs/RenderInfo.h"
 
 class Enemy :
 	public Character
@@ -12,11 +16,16 @@ public:
 	void Release();
 	void Update(double deltaTime);
 	void Render();
+	
+	
+
 protected:
+	
 	
 	float movementSpeed, health, damage;
 
 private:
 	EnemyStateMachine* enemyState;
+	RenderInfoEnemy renderInfo;
 };
 
