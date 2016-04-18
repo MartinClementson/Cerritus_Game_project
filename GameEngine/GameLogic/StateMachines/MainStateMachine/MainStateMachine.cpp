b1e9e9fd8ad8e110cff8ca7edf9e9f477f67800e
Gaffel15 <thegaffel15@gmail.com>
2016-04-18 13:03:20 +0200

    did something

--- GameEngine/GameLogic/StateMachines/MainStateMachine/MainStateMachine.cpp
+++ GameEngine/GameLogic/StateMachines/MainStateMachine/MainStateMachine.cpp
@@ -13,10 +13,34 @@ MainStateMachine::~MainStateMachine()
 
 void MainStateMachine::Update(double deltaTime)
 {
+	if (gameState->isActive)
+	{
+		gameState->Update(deltaTime);
+	}
+	else if (gameOverState->isActive)
+	{
+		gameOverState->Update(deltaTime);
+	}
+	else if (menuState->isActive)
+	{
+		menuState->Update(deltaTime);
+	}
 }
 
 void MainStateMachine::Render()
 {
+	if(gameState->isActive)
+	{
+		gameState->Render();
+	}
+	else if (gameOverState->isActive)
+	{
+		gameOverState->Render();
+	}
+	else if (menuState->isActive)
+	{
+		menuState->Render();
+	}
 }
 
 void MainStateMachine::Initialize()
