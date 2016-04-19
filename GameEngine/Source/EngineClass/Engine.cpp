648f0ee961ad51411e6860a35c296e55b0631c17
Gaffel15 <thegaffel15@gmail.com>
2016-04-19 16:42:48 +0200

    sup

--- GameEngine/Source/EngineClass/Engine.cpp
+++ GameEngine/Source/EngineClass/Engine.cpp
@@ -17,11 +17,11 @@ Engine::~Engine()
 
 void Engine::Initialize(HWND* window, HINSTANCE* hInstance)
 {
-	/*if (!input->Initialize(window,hInstance))
+	if (!input->Initialize(window,hInstance))
 	{
 		MessageBox(0, L"DIRECT INPUT INITILIZATION - FAILED",
 			L"ERROR", MB_OK);
-	}*/
+	}
 	input->Initialize(window, hInstance);
 	graphics->Initialize(window);
 	game->Initialize();
