698f66cb40188fc31392ab18c8d44a705d7b0d17
MartinClementson <martinc.91@hotmail.com>
2016-04-19 17:46:43 +0200

    Created GameTimer

--- GameEngine/GameTimer.h
+++ GameEngine/GameTimer.h
@@ -1,8 +1,32 @@
 #pragma once
+#include "Source\LibIncluder.h"
 class GameTimer
 {
 public:
 	GameTimer();
-	~GameTimer();
+	
+
+
+	float GameTime()const; //in seconds
+	float DeltaTime()const; //in seconds
+	float TotalTime() const; // A timer, can be used for in game timers, level time/ bomb explosion time, animation time. anything 
+
+	void Reset(); //call before message loop
+	void Start(); // call when unpaused
+	void Stop(); // Call when paused
+	void Tick(); //call every frame
+
+	 ~GameTimer();
+private:
+	double mSecondsPerCount;
+	double mDeltaTime;
+
+	__int64 mBaseTime;
+	__int64 mPausedTime;
+	__int64 mStopTime;
+	__int64 mPrevTime;
+	__int64 mCurrTime;
+
+	bool mStopped;
 };
 
