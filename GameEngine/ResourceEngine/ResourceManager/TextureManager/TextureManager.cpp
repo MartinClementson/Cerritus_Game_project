c999726dc413901ed9d5ddd2f5087fbdc2ce3073
David Wigelius <david.wigelius@gmail.com>
2016-04-22 12:26:46 +0200

    made functions that return the id for texture

--- GameEngine/ResourceEngine/ResourceManager/TextureManager/TextureManager.cpp
+++ GameEngine/ResourceEngine/ResourceManager/TextureManager/TextureManager.cpp
@@ -9,22 +9,62 @@ void TextureManager::Release()
 
 int TextureManager::GetDiffuseID(std::string diffuseTex)
 {
-	return 0;
+	if (this->diffuseTex.size == 0 && diffuseTex != "")
+	{
+		this->diffuseTex.push_back(diffuseTex);
+		return 0;
+	}
+	else if (diffuseTex != "")
+	{
+		this->diffuseTex.push_back(diffuseTex);
+		return (this->diffuseTex.size() - 1);
+	}
+	return -1;
 }
 
 int TextureManager::GetNormalID(std::string normalTex)
 {
-	return 0;
+	if (this->normalTex.size == 0 && normalTex != "")
+	{
+		this->normalTex.push_back(normalTex);
+		return 0;
+	}
+	else if (normalTex != "")
+	{
+		this->normalTex.push_back(normalTex);
+		return (this->normalTex.size() - 1);
+	}
+	return -1;
 }
 
 int TextureManager::GetSpecularID(std::string specularTex)
 {
-	return 0;
+	if (this->specularTex.size == 0 && specularTex != "")
+	{
+		this->specularTex.push_back(specularTex);
+		return 0;
+	}
+	else if (specularTex != "")
+	{
+		this->specularTex.push_back(specularTex);
+		return (this->specularTex.size() - 1);
+	}
+	return -1;
 }
 
 int TextureManager::GetGlowID(std::string glowTex)
 {
-	return 0;
+	if (this->glowTex.size == 0 && glowTex != "")
+	{
+		this->glowTex.push_back(glowTex);
+		return 0;
+	}
+	else if (glowTex != "")
+	{
+		this->glowTex.push_back(glowTex);
+		return (this->glowTex.size() - 1);
+	}
+	return -1;
 }
 
 TextureManager::TextureManager()
