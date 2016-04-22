3d30f419de0a61b005d4aa9ddfe3dbf40ffdea63
MartinClementson <martinc.91@hotmail.com>
2016-04-22 12:53:44 +0200

    Added physics for player movement

--- GameEngine/GameLogic/Character/Player/Player.h
+++ GameEngine/GameLogic/Character/Player/Player.h
@@ -6,8 +6,57 @@
 #include "../../../Enumerations/Enumerations.h"
 #include "../../../Structs/RenderInfo.h"
 
+struct Vec3
+{
+	float x;
+	float y;
+	float z;
+
+	Vec3() { x = 0; y = 0; z = 0; };
+	Vec3(float x, float y, float z) { this->x = x; this->y = y; this->z = z; };
+
+	Vec3 operator+(Vec3 v)
+	{
+		return Vec3(this->x + v.x, this->y + v.y, this->z + v.z);
+	}
 
 
+	Vec3 operator*(const float& f)
+	{
+		return Vec3(this->x * f, this->y * f, this->z * f);
+	}
+
+	inline float LengthSqrd()
+	{
+		if (x + y + z != 0)
+			return (x*x + y*y + z*z);
+		else
+			return NULL;
+	}
+	float Length()
+	{
+		float l = LengthSqrd();
+		if(l != NULL)
+			return sqrt(l);
+		else 
+			return NULL;
+	}
+
+	Vec3 Normalize()
+	{
+		float l = Length();
+		
+		
+		return Vec3 (x /= l,		y /= l,		z /= l);
+	}
+
+
+
+};
+
+
+
+#define VELOCITY_MAX 4.0f
 class Player :
 	public Character
 {
@@ -37,5 +86,16 @@ private:
 
 	XMFLOAT3 direction;
 	//Collision();
+
+
+
+	Vec3 velocity			= Vec3(0.1f,0.1f,0.1f);
+	Vec3 acceleration		= Vec3(0.0f, 0.0f, 0.0f);
+	float fallOfFactor		= 8.0f; //the bigger number, the faster fallOff , this is like friction
+	float maxAcceleration	= 5.0f;
+
+	
+
 };
+
 
