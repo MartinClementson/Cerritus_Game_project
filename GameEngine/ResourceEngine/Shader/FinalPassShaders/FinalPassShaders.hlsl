e3cf96cb933a9dcf2cc06cbde5c3bf3fe8305d8c
MartinClementson <martinc.91@hotmail.com>
2016-04-27 21:04:57 +0200

    Added another sampler

--- GameEngine/ResourceEngine/Shader/FinalPassShaders/FinalPassShaders.hlsl
+++ GameEngine/ResourceEngine/Shader/FinalPassShaders/FinalPassShaders.hlsl
@@ -58,7 +58,8 @@ VS_OUT VS_main( VS_IN input)
 }
 
 
-SamplerState SampleType;
+SamplerState linearSampler		 : register(s0);
+SamplerState pointSampler		 : register(s1);
 
 Texture2D		diffuseTexture		 : register(t0);
 Texture2D		specularTexture		 : register(t1);
@@ -103,15 +104,15 @@ float sampleShadowStencils(float4 worldPos)
 		projectTexCoord.x = projectTexCoord.x * 0.5f + 0.5f;
 		projectTexCoord.y = projectTexCoord.y * -0.5f + 0.5f;
 
-		depthValue = shadowTex.Sample(SampleType, float3(projectTexCoord.xy, i)).r + bias;
+		depthValue = shadowTex.Sample(linearSampler, float3(projectTexCoord.xy, i)).r + bias;
 
 		//float tempSample = shadowTex.Sample(samplerTypeState, float3(projectTexCoord, i)).r
 
 		float dx = 1.0f / SMAP_SIZE;
-		float s0 = (shadowTex.Sample(SampleType, float3(projectTexCoord, i)).r + bias < lightDepthValue) ? 0.0f : 1.0f;
-		float s1 = (shadowTex.Sample(SampleType, float3(projectTexCoord, i) + float3(dx, 0.0f, 0.0f)).r + bias < lightDepthValue) ? 0.0f : 1.0f;
-		float s2 = (shadowTex.Sample(SampleType, float3(projectTexCoord, i) + float3(0.0f, dx, 0.0f)).r + bias < lightDepthValue) ? 0.0f : 1.0f;
-		float s3 = (shadowTex.Sample(SampleType, float3(projectTexCoord, i) + float3(dx, dx, 0.0f)).r + bias < lightDepthValue) ? 0.0f : 1.0f;
+		float s0 = (shadowTex.Sample(linearSampler, float3(projectTexCoord, i)).r + bias < lightDepthValue) ? 0.0f : 1.0f;
+		float s1 = (shadowTex.Sample(linearSampler, float3(projectTexCoord, i) + float3(dx, 0.0f, 0.0f)).r + bias < lightDepthValue) ? 0.0f : 1.0f;
+		float s2 = (shadowTex.Sample(linearSampler, float3(projectTexCoord, i) + float3(0.0f, dx, 0.0f)).r + bias < lightDepthValue) ? 0.0f : 1.0f;
+		float s3 = (shadowTex.Sample(linearSampler, float3(projectTexCoord, i) + float3(dx, dx, 0.0f)).r + bias < lightDepthValue) ? 0.0f : 1.0f;
 
 		float2 texelpos = projectTexCoord * SMAP_SIZE;
 		float2 lerps = frac(texelpos);
@@ -132,16 +133,16 @@ float sampleShadowStencils(float4 worldPos)
 float4 PS_main(VS_OUT input) : SV_TARGET
 {
 	float4 ambient			= float4(0.2f,0.2f,0.2f,1.0f); // Hardcoded ambient. 
-	float depthSample		= depthTexture.Sample(SampleType, input.Uv);
+	float depthSample		= depthTexture.Sample(pointSampler, input.Uv);
 
 	float4 depthPrepare		= mul(float4(input.Uv.x *2.0f - 1.0f, input.Uv.y * -2.0f + 1.0f, depthSample, 1.0f), invViewProjMatrix);
 	
-	float3 worldPos			= positionTexture.Sample(SampleType, input.Uv); //depthPrepare / depthPrepare.w;
+	float3 worldPos			= positionTexture.Sample(pointSampler, input.Uv); //depthPrepare / depthPrepare.w;
 
 
-	float4 diffuseSample			= diffuseTexture.Sample(SampleType,input.Uv);
-	float4 normal					= normalTexture.Sample(SampleType, input.Uv);
-	float4 specular					= specularTexture.Sample(SampleType, input.Uv);
+	float4 diffuseSample			= diffuseTexture.Sample(pointSampler,input.Uv);
+	float4 normal					= normalTexture.Sample(pointSampler, input.Uv);
+	float4 specular					= specularTexture.Sample(pointSampler, input.Uv);
 	float4 shadow					= sampleShadowStencils(float4(worldPos, 1.0f));
 
 	//The light ray from the vert position to the light
@@ -163,41 +164,34 @@ float4 PS_main(VS_OUT input) : SV_TARGET
 							
 	float3 KS				= specular.xyz;									 //This is the color of the specularity. For now it is set as the light color
 							
-	float3 specularLight	= { float3(1,1,1) * pow(max(dot(r,v),0.0f),shinyPower) };
+	float3 specularLight	= { KS * pow(max(dot(r,v),0.0f),shinyPower) };
 				
-	//float lRadi				= 40.0f;
-	//float denom				= rayLength / lRadi + 1.0f;
-	//float attenuation		= min(lerp(1.0f, 0.0f, rayLength / (lRadi * 2.0f)), 1.0f / (denom * denom));
+	
 
 	float4 lightDiffuse		= saturate( lightColor * fDot ) + float4(specularLight, 1.0f);
 	
 	float3 spotDir			= normalize(lightDir - lightPosition);
 
-	lightDiffuse			*= pow(max(dot(-vRay, spotDir), 0.0f), 20.0f);
+//	lightDiffuse			*= pow(max(dot(-vRay, spotDir), 0.0f), 30.0f);
 
-	/*float radio			= 40.0f;
+	float radio			= 80.0f;
 	float lightLength	= length(lightDir - worldPos);
 	if (lightLength > radio)
 		lightDiffuse *= 0.0;
 	else
-		lightDiffuse *= ((radio - lightLength) / radio);*/
+		lightDiffuse *= ((radio - lightLength) / radio);
 
-	//float3 finalCol			 = (lightDiffuse + ambient);
-
-	//finalCol				 = diffuseSample.xyz * finalCol; // texture * (lightDiffuse + ambient)
-
-	//finalCol				 = saturate(finalCol + specularLight);
-
-	//finalCol = finalCol *
+	
 
 
 	//========== Screen Space Ambient Occlusion =============
-	// USED FOR TESTING SSAO ONLY, SUCKY VERSION, JOCKE PLEZ HALP
+	 //quite fake. Use AO maps if possible. dis should be for objects without AO maps
 	float ssao = 0.0f;
 
-	float2 vec[8] = { float2(1.0f, 0.0f), float2(-1.0f, 0.0f),
-		float2(0.0f, 1.0f), float2(0.0f, -1.0f),
-		float2(0.707f, 0.707f), float2(-0.707f, -0.707f),
+	float2 vec[8] = { 
+		float2(1.0f, 0.0f),		 float2(-1.0f, 0.0f),
+		float2(0.0f, 1.0f),		 float2(0.0f, -1.0f),
+		float2(0.707f, 0.707f),  float2(-0.707f, -0.707f),
 		float2(-0.707f, 0.707f), float2(0.707f, -0.707f), };
 
 	[unroll]
@@ -206,7 +200,7 @@ float4 PS_main(VS_OUT input) : SV_TARGET
 		for (float k = 0.0f; k < 1.01f; k += 0.5f)
 		{
 			float2  sampleVec = input.Uv + vec[j] * SSAO_RAD * k;
-			float	ssaoDepthSample = depthTexture.Sample(SampleType, sampleVec);
+			float	ssaoDepthSample = depthTexture.Sample(pointSampler, sampleVec);
 			float4  ssaoDepthPrepare = mul(float4(sampleVec.x * 2.0f - 1.0f, sampleVec.y * -2.0f + 1.0f, ssaoDepthSample, 1.0f), invViewProjMatrix);
 			float3 dif = ssaoDepthPrepare.xyz / ssaoDepthPrepare.w - worldPos;
 			float l = length(dif) * 0.05f;
@@ -220,12 +214,11 @@ float4 PS_main(VS_OUT input) : SV_TARGET
 
 
 
-		//NEW
+	float4 finalCol = diffuseSample * ( ambient * ssao + (lightDiffuse * shadow) );
+	finalCol.r += diffuseSample.a; //Laser point color
+	finalCol = saturate(finalCol);
 
-		float4 finalCol = diffuseSample * ( ambient * ssao + (lightDiffuse * shadow) );
-		finalCol = saturate(finalCol);
 
-	//float4 col				 = { finalCol,1.0 };		 //depthTexture.Sample(SampleType,input.Uv);
 
 
 
