cd821f2f357cad54c46f1c90fc1e70ef3adf9578
David Wigelius <david.wigelius@gmail.com>
2016-05-04 16:12:45 +0200

    hardcoded regular bullets

--- GameEngine/ResourceEngine/Graphics/Graphics.cpp
+++ GameEngine/ResourceEngine/Graphics/Graphics.cpp
@@ -383,8 +383,8 @@ void Graphics::CullGeometry()
 				//this->instancedWorldDataPerFrame[PROJECTILE_INSTANCED][projectileIndex].worldMatrix = CalculateWorldMatrix(&this->gameObjects->at(i)->position, &this->gameObjects->at(i)->rotation);
 				billBoardArray[projectileIndex].worldPos  = this->gameObjects->at(i)->position;
 				billBoardArray[projectileIndex].direction = this->gameObjects->at(i)->direction;
-				billBoardArray[projectileIndex].height    = 1.0f;
-				billBoardArray[projectileIndex].width     = 0.5f;
+				billBoardArray[projectileIndex].height    = 3.0f;
+				billBoardArray[projectileIndex].width     = 0.15f;
 				instancesToRender[PROJECTILE_INSTANCED]  += 1;
 				projectileIndex							 += 1;
 				this->gameObjects->at(i)->render		  = false; //We don't want to render this with nonInstance rendering
