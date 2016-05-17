a14ad99cd5f48d1bc3a9a7ed16b3d7ab762186dd
David Wigelius <david.wigelius@gmail.com>
2016-05-17 11:18:10 +0200

    added funtions

--- GameEngine/ResourceEngine/anttweakbar/AntTweakBar.cpp
+++ GameEngine/ResourceEngine/anttweakbar/AntTweakBar.cpp
@@ -10,3 +10,20 @@ AntTweakBar::AntTweakBar()
 AntTweakBar::~AntTweakBar()
 {
 }
+
+void AntTweakBar::Initialize(ID3D11Device * gDevice)
+{
+	gMyBar = TwNewBar("Cerritus TweakBar");
+
+	TwInit(TW_DIRECT3D11, gDevice);
+	TwWindowSize(WIN_HEIGHT, WIN_WIDTH);
+}
+
+void AntTweakBar::Update()
+{
+	TwDraw();
+}
+
+void AntTweakBar::Release()
+{
+}
--- GameEngine/ResourceEngine/anttweakbar/AntTweakBar.h
+++ GameEngine/ResourceEngine/anttweakbar/AntTweakBar.h
@@ -1,8 +1,17 @@
 #pragma once
+#include <AntTweakBar.h>
+#include "../../Source/Constants.h"
+#include "../../Source/LibIncluder.h"
 class AntTweakBar
 {
 public:
 	AntTweakBar();
 	~AntTweakBar();
+
+	void Initialize(ID3D11Device* gDevice);
+	void Update();
+	void Release();
+private:
+	TwBar *gMyBar;
 };
 
