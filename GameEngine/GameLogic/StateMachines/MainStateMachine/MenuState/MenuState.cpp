c611151bb7f920c178fef104675864edc82e9b95
Kaptenotto <johancarlphilipottosson@gmail.com>
2016-05-20 17:26:24 +0200

    c

--- GameEngine/GameLogic/StateMachines/MainStateMachine/MenuState/MenuState.cpp
+++ GameEngine/GameLogic/StateMachines/MainStateMachine/MenuState/MenuState.cpp
@@ -42,7 +42,7 @@ void MenuState::Update(double deltaTime)
 	if (vx > Maxex && vy < Maxey && vx < Minex && vy > Miney)
 	{
 		{
-			if (input->isMouseClicked(MOUSE_LEFT) && time > 2.0f)
+			if (input->isMouseClicked(MOUSE_LEFT) && time > 4.0f)
 			{
 				//fix later
 				DestroyWindow(GetActiveWindow());
